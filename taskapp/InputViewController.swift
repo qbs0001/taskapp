f02615c32a43e0cd427e8435c4584f0c9a394239
qbs0001 <qbs0001@sample.com>
2020-05-02 14:18:21 +0900

    gitignore対応まで完了

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 0001 QBS on 2020/05/02.
+//  Copyright © 2020 qbs0001. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextfield: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -34,6 +34,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     // 各セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "cellSegue",sender: nil) // ←追加する
+        
     }
 
     // セルが削除が可能なことを伝えるメソッド
